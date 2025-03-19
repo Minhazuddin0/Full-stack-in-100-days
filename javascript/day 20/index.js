1649b8f5a1020678c255e65713ab4776fc0a50d7
Minhaz Uddin <minhazrock1@gmail.com>
2025-03-20 03:31:18 +0530

    update

--- javascript/day 20/index.js
+++ javascript/day 20/index.js
@@ -5,5 +5,21 @@ let a =true;
 console.log(a);
 
 const num =12;
+// num=13
 
-num =13;
+
+// check variable print types
+let lastName = 'minhaz';
+console.log(lastName)
+
+lastName =5;
+console.log(lastName);
+
+lastName =true;
+console.log(lastName) 
+
+let b=1;
+let c=3;
+let d=b+c;
+
+console.log(d)
