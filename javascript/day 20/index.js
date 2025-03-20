feb4f9af82a64bdba1c8209b58772331223b9236
Minhaz Uddin <minhazrock1@gmail.com>
2025-03-20 16:01:43 +0530

    update

--- javascript/day 20/index.js
+++ javascript/day 20/index.js
@@ -70,14 +70,29 @@ console.log(status)
 // }
 
 
-for(let i = 1; i<=5; i=i+1) {
-    console.log("min"+i);
-}
+ 
+// for loop
+
+// for(let i = 1; i<=5; i=i+1) {
+//     console.log("min"+i);
+// }
+
+// for while loop
+
+// let x=1;
+
+// while (x<=5) {
+//     console.log(x);
+//     x++;
+// }
 
 
-let x=1;
+// for do while loop
 
-while (x<=5) {
-    console.log(x);
-    x++;
-}
+let y=1;
+
+do{
+    console.log(y);
+    y++;
+} while (y<10) ;
+    
