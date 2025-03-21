498bd07ee31bdc9212e1e6fa339789d71f4bc9d8
Minhaz Uddin <minhazrock1@gmail.com>
2025-03-22 01:29:11 +0530

    update

--- javascript/day 21/index.js
+++ javascript/day 21/index.js
@@ -13,16 +13,42 @@ let rectangle ={
 
 // factory function
 
-function createRectangle() {
+// function createRectangle(len, bre) {
 
-    return rectangle ={
-        length:1,
-        breadth:2,
+//     return rectangle  ={
+//         length:len,
+//         breadth:bre,
     
-        draw()  {
-            console.log('drawing');
-        }
-    };
+//         draw()  {
+//             console.log('drawing');
+//         }
+//     };
 
+// }
+//  let rectangleObj1 = createRectangle(5 ,4);
+//  let rectangle2 = createRectangle(2 ,3);
+//  let rectangle3 = createRectangle(7 ,9);
+
+// Camelcase -> numberOfStudents
+// constructor function --> pascal notation -> first letter of every word is capital -> NumberOfStudents
+
+// constructor function -> prop/methods -> initialize/define
+
+
+function Rectangle(len ,bre){
+    this.length =len;
+    this.breadth =bre;
+    this.draw = function(){
+        console.log('drawing');
+    }
 }
- let rectangleObj1 = creatrectangle();
+
+// object creation using constructor function
+
+let rectangleObject = new Rectangle(4,6);
+
+rectangleObject.color = 'yellow';
+console.log(rectangleObject);
+
+delete rectangleObject.color;
+console.log(rectangleObject);
