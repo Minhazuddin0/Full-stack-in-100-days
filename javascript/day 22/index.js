effe5fc1fa3af7ab4f036ed1cb87b8cf4e9975e2
Minhaz Uddin <minhazrock1@gmail.com>
2025-03-27 03:07:01 +0530

    update

--- javascript/day 22/index.js
+++ javascript/day 22/index.js
@@ -1,7 +1,18 @@
 console.log('hello world');
 
 let lastName= 'minhaz';
+
 let firstName = new String ('minni');
 
-let message='this is my first message';
+let message= 
+`this ${lastName},
+is my first 
+message`;
+
+console.log(message);
+
+// let words = message.split('  ');
+
+// console.log(words);
+
 
