c6f3c0609ad5559fb098ca21d1476cc7f0ec07a7
Minhaz Uddin <minhazrock1@gmail.com>
2025-03-30 03:54:59 +0530

    update

--- javascript/day 22/index.js
+++ javascript/day 22/index.js
@@ -1,24 +1,33 @@
-console.log('hello world');
+// console.log('hello world');
 
-let lastName= 'minhaz';
+// let lastName= 'minhaz';
 
-let firstName = new String ('minni');
+// let firstName = new String ('minni');
 
-let message= 
-`this ${lastName},
-is my first 
-message`;
+// let message= 
+// `this ${lastName},
+// is my first 
+// message`;
 
-console.log(message);
+// console.log(message);
 
-// let words = message.split('  ');
+// // let words = message.split('  ');
 
-// console.log(words);
+// // console.log(words);
 
 
-let date =new Date();
+// let date =new Date();
 
-let date2 =new Date('January 21 1998 07:15 ');
+// let date2 =new Date('January 21 1998 07:15 ');
 
-let date3 =new Date(2002, 5, 21, 7)
-console.log(date3);
+// let date3 =new Date(2002, 5, 21, 7)
+
+// console.log(date3);
+
+ 
+let numbers = [1,4,5,7 ];
+ console.log(numbers);
+
+//  end --> push
+// begin --> unshift
+// middle --> splice
