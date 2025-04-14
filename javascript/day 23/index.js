04e10c60e8d0252f481cffacda61d26058cfbb14
Minhaz Uddin <minhazrock1@gmail.com>
2025-04-14 16:41:03 +0530

    update

--- javascript/day 23/index.js
+++ javascript/day 23/index.js
@@ -1,12 +1,30 @@
 // console.log('kaise ho');
 
+// run();
 
 // function declaration
 
-function run (){
-    console.log('running');
-}
+// function run (){
+//     console.log('running');
+// }
 
 // function call or invoke
 
-run();
+
+
+// Named function assignment
+// let stand =function walk(){
+//     console.log('walking');
+// }
+
+// Anonymous function assignment
+// let stand2 =function(){
+//     console.log('walking');
+// }
+
+// stand();
+
+// let jump= stand;
+
+// jump();
+// stand2();
