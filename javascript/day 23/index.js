ff8277be71d7a81a8049b6dac743194512841a42
Minhaz Uddin <minhazrock1@gmail.com>
2025-04-15 16:58:08 +0530

    update

--- javascript/day 23/index.js
+++ javascript/day 23/index.js
@@ -135,3 +135,14 @@
 // function b(){
 //     const ab =5;
 // }
+
+let arr = [1,2,3,4,5];
+// let total = 0;
+
+// for (let value of arr)
+//     total=total+value;
+// console.log(total);
+
+let totalSum = arr.reduce((accumulator , currentvalue) => accumulator + currentvalue, 0); 
+console.log('Print total sum');
+console.log(totalSum);
