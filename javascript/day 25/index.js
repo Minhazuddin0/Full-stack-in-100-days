421385333d24027fb7b0f11afd77972e782f8bc8
Minhaz Uddin <minhazrock1@gmail.com>
2025-04-24 11:30:26 +0530

    update

--- javascript/day 25/index.js
+++ javascript/day 25/index.js
@@ -5,16 +5,16 @@
 // document.addEventListener('click', eventlistener);
 // document.removeEventListener('click', eventlistener);
 
-const content = document.querySelector('#wrapper');
+// const content = document.querySelector('#wrapper');
 
-content.addEventListener('click' , function (event){
-    console.log(event);
-}) 
+// content.addEventListener('click' , function (event){
+//     console.log(event);
+// }) 
 
  let links = document.querySelectorAll('a');
  let thirdlink = links[2];
 
- thirdlink = addEventListener('click' , function(event){
+ thirdlink.addEventListener('click' , function(event){
     event.preventDefault();
     console.log('prevent event open nhi hoga')
  });
