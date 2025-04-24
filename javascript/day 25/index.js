dd6a7ddf239566e4cda03980ce08b08cc96714dc
Minhaz Uddin <minhazrock1@gmail.com>
2025-04-24 12:34:18 +0530

    update

--- javascript/day 25/index.js
+++ javascript/day 25/index.js
@@ -11,10 +11,37 @@
 //     console.log(event);
 // }) 
 
- let links = document.querySelectorAll('a');
- let thirdlink = links[2];
+//  let links = document.querySelectorAll('a');
+//  let thirdlink = links[2];
 
- thirdlink.addEventListener('click' , function(event){
-    event.preventDefault();
-    console.log('prevent event open nhi hoga')
- });
+//  thirdlink.addEventListener('click' , function(event){
+//     event.preventDefault();
+//     console.log('prevent event open nhi hoga')
+//  });
+
+// let myDiv = document.createElement('div');
+
+// function parastatus (event){
+//     console.log('para' + event.target.textContent);
+// }
+
+// myDiv.addEventListener('click', parastatus );
+// for (let i=1; i<=100; i++){
+//     let newElement = document.createElement('p');
+//     newElement.textContent = 'this is para' + i;
+
+    
+//     myDiv.appendChild(newElement);
+
+// }
+
+// document.body.appendChild(myDiv);  
+
+let element = document.querySelector('#wrapper');
+
+element.addEventListener('click', function(event){
+    if(event.target.nodeName === 'SPAN'){
+    console.log('span per click hua ' + event.target.textContent);
+    }
+    
+});
