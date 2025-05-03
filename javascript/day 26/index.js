b104f2dccd8f468e4c80a2f87bd7b00db62cbfae
Minhaz Uddin <minhazrock1@gmail.com>
2025-05-03 11:57:19 +0530

    update

---
+++ javascript/day 26/index.js
@@ -0,0 +1,27 @@
+// adding 100 para
+const t1 = performance.now();
+for(let i=1; i<=100; i++){
+    let newElement = document.createElement('p');
+    newElement.textContent = 'this is para' + i;
+
+    document.body.appendChild(newElement);
+}
+
+const t2 = performance.now();
+console.log("Load time first " + (t2 - t1) + "ms")
+
+// optimising a bit
+const t3 = performance.now();
+let myDiv = document.createElement('div');
+
+for(let i=1; i<=100; i++){
+    let element= document.createElement('p');
+    element.textContent = 'this is para'+ i ;
+
+    myDiv.appendChild(element);
+}
+
+document.body.appendChild(myDiv);
+const t4 = performance.now();
+
+console.log("Load time second " + (t4 - t3) + "ms")
