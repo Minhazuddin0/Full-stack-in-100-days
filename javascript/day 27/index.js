4d0ebad239d1c4ba3de82d0845998a191e93b8d3
Minhaz Uddin <minhazrock1@gmail.com>
2025-05-19 10:42:48 +0530

    update

--- javascript/day 27/index.js
+++ javascript/day 27/index.js
@@ -1,21 +1,41 @@
-let meraPromise1 = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log('i am promise1');
-    },5000);
-    // resolve(123456789);
-    reject(new Error('new error aa gya hai '))
-})
-meraPromise1.then((value) => {console.log (value)});
-
-meraPromise1.catch((error) => {console.log('this is error')});
-
-
-// let meraPromise2 = new Promise(function(resolve, reject){
+// let meraPromise1 = new Promise(function(resolve, reject){
 //     setTimeout(function(){
-//         console.log('i am promise2');
-//     },3000);
-//     // resolve(2233)
-//     // reject(new Error('new error aa gya hai '))
+//         console.log('i am promise1');
+//     },5000);
+//     // resolve(123456789);
+//     reject(new Error('new error aa gya hai '))
 // })
+// meraPromise1.then((value) => {console.log (value)});
 
-console.log('pehla');
+// meraPromise1.catch((error) => {console.log('this is error')});
+
+
+// // let meraPromise2 = new Promise(function(resolve, reject){
+// //     setTimeout(function(){
+// //         console.log('i am promise2');
+// //     },3000);
+// //     // resolve(2233)
+// //     // reject(new Error('new error aa gya hai '))
+// // })
+
+// console.log('pehla');
+
+
+let promise1 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        console.log('timeout1 started');
+    }, 2000);
+    resolve(true);
+})
+
+let output = promise1.then(() => {
+    let promise2 = new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            console.log('timeout2 started');
+        }, 3000);
+        resolve("promise 2 resolve");
+    })
+
+    return promise2;
+})
+output.then((value) => console.log( value));
