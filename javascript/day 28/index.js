e4658831889cad37d2e5a91f63c63ab3d800996f
Minhaz Uddin <minhazrock1@gmail.com>
2025-05-19 18:52:49 +0530

    update

--- javascript/day 28/index.js
+++ javascript/day 28/index.js
@@ -1,16 +1,24 @@
 const countValue = document.querySelector('#counter');
 
 const increment = () => {
+
     // get value from UI
-    let value = parseInt(countValue.innertext); 
+    let value = parseInt(countValue.innerText); 
     // update the value
-    value = value+1;
+    value = value + 1;
     // set the value onto UI
-    countValue.innertext =value ;
+    countValue.innerText = value ;
 
 };
 
 
 const decrement = () => {
+
+     // get value from UI
+    let value = parseInt(countValue.innerText); 
+    // update the value
+    value = value - 1;
+    // set the value onto UI
+    countValue.innerText = value ;
 
 };
