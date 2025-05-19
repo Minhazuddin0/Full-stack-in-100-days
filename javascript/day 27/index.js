692caba07646f1f980fe0b5bf4af456c326f33ec
Minhaz Uddin <minhazrock1@gmail.com>
2025-05-19 10:18:21 +0530

    update

--- javascript/day 27/index.js
+++ javascript/day 27/index.js
@@ -1,1 +1,19 @@
-console.log('minhaz here')
+let meraPromise1 = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log('i am promise1');
+    },5000);
+    // resolve(123456789);
+    reject(new Error('new error aa gya hai '))
+})
+meraPromise1.then((value) => {console.log (value)});
+
+
+// let meraPromise2 = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log('i am promise2');
+//     },3000);
+//     // resolve(2233)
+//     // reject(new Error('new error aa gya hai '))
+// })
+
+console.log('pehla');
